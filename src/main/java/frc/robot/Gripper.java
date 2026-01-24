09abd48436d732dca059110b39961aa9c0c721bf
novarae126 <nova.rae126@gmail.com>
2026-01-24 13:14:00 -0500

    Refined arm code, and added pnumatics and position based angle code.

--- src/main/java/frc/robot/Gripper.java
+++ src/main/java/frc/robot/Gripper.java
@@ -1,6 +1,8 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.PneumaticHub;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -8,16 +10,19 @@ public class Gripper extends SubsystemBase{
     private Solenoid gripSolenoid;
     private Solenoid releaseSolenoid;
     private PneumaticHub hub;
-    public Pnuematics pnuematics = new Pnuematics();
+    private Compressor compressor;
 
     public Gripper(){
         hub = new PneumaticHub(8);
         gripSolenoid = hub.makeSolenoid(0);
         releaseSolenoid = hub.makeSolenoid(1);
+        compressor = new Compressor(8, PneumaticsModuleType.REVPH);
+        compressor.enableDigital();
     }
 
     public void grip(boolean gripIn){
-        gripSolenoid.set(!gripIn);
-        releaseSolenoid.set(gripIn);
+        gripSolenoid.set(gripIn);
+        releaseSolenoid.set(!gripIn);
+    
     }
 }
