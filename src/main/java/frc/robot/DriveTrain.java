aa482ccef10b607cbb284c0ad58438fcd845c146
voidb-o-t <samthekid2022@gmail.com>
2026-01-31 14:05:28 -0500

    formated code

--- src/main/java/frc/robot/DriveTrain.java
+++ src/main/java/frc/robot/DriveTrain.java
@@ -19,7 +19,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class DriveTrain extends SubsystemBase {
 
   private final RelativeEncoder leftEncoder;
-    private final RelativeEncoder rightEncoder;
+  private final RelativeEncoder rightEncoder;
   private final SparkMax leftMotor = new SparkMax(3, MotorType.kBrushless);
   private final SparkMax rightMotor = new SparkMax(1, MotorType.kBrushless);
   private final SparkMax leftMotorFollow = new SparkMax(4, MotorType.kBrushless);
